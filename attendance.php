2e918e54682af75fd81b84c0f02e13dd68e65423
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-03-03 15:45:25 +0300

    Initial Commit

---
+++ attendance.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<title>FSM Systems</title>
+	<meta name="p:domain_verify" content="17feac793e54b3dcc20cda8adbf7509b"/>
+	<link rel="icon" type="image/png" href="img/favicon.png">
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<meta name="keywords" content="fsm, attendance, time, employee, workers, company">
+  	
+  	<?php
+	include "libraries.html"
+	?>
+	
+	<script>
+  		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  		ga('create', 'UA-74087442-1', 'auto');
+  		ga('send', 'pageview');
+	</script>
+	<link rel="stylesheet" href="css/main.css" />
+</head>
+<body data-spy="scroll">
+<?php
+include "header.html"
+?>
+<br>
+
+<div class="container-fluid text-center bg-1" id="divinfo">
+	<h3>Need an affordable and simple solution to track your employee attendance? </h3>
+	<h3>Want to avoid forgery and cheating? </h3>
+	<!-- <img src="img/attendance.png" alt="FSM Attendance" class="img-responsive center-block">-->
+
+		<div class="row">
+		    <div class="col-sm-4">
+		      <p>
+		      Your employee will use his mobile phone browser to clock in at his assigned area. 
+		      No username or passwords required as every phone has its unique "finger print". This will ensure no forgery is done with passwords.
+		      Get your employee to use his own phone so he will not share it with other co-workers!
+		      </p>
+		      <img src="img/mobile.png" class="img-responsive" style="width:100%"  alt="Image">
+		    </div>
+		    <div class="col-sm-4">
+		      <p>
+		      		Create your own areas on maps where your employees can clock in. Predefine them very precisely in a visual way.
+		      		You can then bind each area to a specific employee so he is authorised only to clock in specific geographical areas.
+		      </p>
+		      <img src="img/areas2.png" class="img-responsive" style="width:100%"  alt="Image">
+		    </div>
+		    <div class="col-sm-4"> 
+		      <p>
+		      		Check the reports daily to see if the employee has clocked in into the correct area. Add notes to a specific clock date. 
+		      		If not in the area you can easily pinpoint where the employee has clocked in and take disciplinary action.
+		      	</p>
+		      <img src="img/timesheet2.png" class="img-responsive" style="width:100%"  alt="Image">
+		      <p>
+		      		Click on the date detail to pinpoint the exact locaton where the timing in was done.
+		      	</p>
+		      	<img src="img/pinpoint.png" class="img-responsive" style="width:100%"  alt="Image">
+		    </div>
+	  </div>
+</div>
+
+<div class="container-fluid text-center bg-2" id="divdemo" style="min-height: 100vh; padding-top: 10%">
+	<h3>How to test FSM Attendance for <u>free</u> (3 days).</h3>
+	<div class="row">
+		<div class="col-sm-4">
+			<p>
+			Open the <a href="http://fsm.co.tz/att" target="_new" data-toggle="tooltip" title="https://fsm.co.tz/att">FSM Attendance Mobile Clocking</a> on your mobile phone browser and click on "REGISTER" to bind your smartphone to the system.
+			Allow the system to access your location.
+			</p>
+			<img src="img/register2.png" class="img-responsive img-rounded" style="width:100%"  alt="Image" data-toggle="tooltip" title="Click on Register to activate the Mobile User">
+		</div>
+		<div class="col-sm-4">
+			<p>
+			Clock in/out to save data in the system for analysis. Remember that all the actions can be done <b><u>ONCE</u></b> a day for security reasons!
+			</p>
+			<img src="img/clockinout.png" class="img-responsive img-rounded" style="width:100%"  alt="Image" data-toggle="tooltip" title="Use the actions available to clock your attendance">
+		</div>
+		<div class="col-sm-4">
+			<p>
+			Open the <a href="https://fsm.co.tz/att/main.php" target="_new" data-toggle="tooltip" title="https://fsm.co.tz/att/main.php">FSM Attendance Control Panel</a> on your browser and start analysing your data!
+			</p>
+			<img src="img/analyse.png" class="img-responsive img-rounded" style="width:100%"  alt="Image" data-toggle="tooltip" title="See the performance on the Control Panel">
+		</div>
+	</div>
+	<h4>Short explanation of the concept:</h4>
+	<p>
+		A user will be able to register one browser. A secret key will be stored on the smartphone which will be impossible to be changed. The user will have to ALWAYS use the same browser
+		and phone to register his actions in the system. If he uses another phone he will have to go through the whole process of registering and the double entries will appear.
+		He will be able to clock attendance actions only ONE TIME a day. All clocking actions have to be completed sequentially. 
+	</p>
+	<p>
+		Attendance areas are the physical locations where the user will be able to clock in. These can be simple drawn and named on the map in the Control Panel Area.
+		From the Employees section a user can be bound to specific areas. Clocking in an area will generate a green icon whereas clocking out of an assigned area will create a 
+		red icon for easy identification of employees breaching the rules.
+	</p>
+</div>
+
+<div class="container-fluid text-center bg-1" id="divprice" style="min-height: 100vh; padding-top: 10%">
+	<h2>FSM Attendance is your affordable solution!</h2>
+	<h3><u>Company package @ USD. 250/Year flat fee</u></h3>
+		<p>
+		Includes subdomain https://your_comany.fsm.co.tz for easy access			
+		</p>
+		
+		<h3 class="text-primary">Full access to the system for 1 year.</h3>
+		<h3 class="text-primary">Unlimited number of users/employees.</h3>
+		<h3 class="text-primary">Unlimited number of geographical areas.</h3>
+		<h3 class="text-primary">Phone/Email support</h3>
+		<br>
+		<h4 class="text-warning">Can be installed on your own webserver if required. Different prices apply. Inquire for more information.</h4>
+		<br>
+		Call us for any extra information required. All numbers available on the <a id="contacts1">contacts</a> page.
+</div>
+
+<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+
+
+<footer class="container-fluid bg-4 text-center footer">
+  &copy; 2017 FSM Systems
+</footer>
+
+<script type="text/javascript">
+$(document).ready(function () {
+	// Bootstrap tooltip
+	$(document).ready(function(){
+   		$('[data-toggle="tooltip"]').tooltip(); 
+	});	
+	
+	// Hide bootstrap menu when in mobile after clicking
+	$('.nav a').on('click', function(){
+		$('.navbar-toggle').click() //bootstrap 3.x by Richard
+	});
+	
+	// Scroll to top
+	$(window).scroll(function () {
+	 	if ($(this).scrollTop() > 50) {
+	       $('#back-to-top').fadeIn();
+	   } else {
+	       $('#back-to-top').fadeOut();
+	   }
+	 });
+	 // scroll body to 0px on click
+	 $('#back-to-top').click(function () {
+	 	$('#back-to-top').tooltip('hide');
+	 	$('body,html').animate({
+			scrollTop: 0
+		}, 600);
+	   return false;
+	});
+	  
+	$('#back-to-top').tooltip('show');
+	
+	$("#info").click(function() {
+	    $('html, body').animate({
+	        scrollTop: 0
+	    }, 600);
+	});
+	
+	$("#demo").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#divdemo").offset().top
+	    }, 600);
+	});
+	
+	$("#homelogo").click(function() {
+	    $('html, body').animate({
+	        scrollTop: 0
+	    }, 600);
+	});
+	
+	$("#price").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#divprice").offset().top
+	    }, 600);
+	});
+	
+	$("#contacts, #contacts1").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#divcontacts").offset().top
+	    }, 600);
+	});
+});
+</script>
+</body>
+<html>
---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+ <head>
+ 	<title>FSM Systems</title>
+	<meta name="p:domain_verify" content="17feac793e54b3dcc20cda8adbf7509b"/>
+	<link rel="icon" type="image/png" href="img/favicon.png">
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<meta name="keywords" content="fsm, attendance, time, employee, workers, company">
+  	
+  	<?php
+	include "libraries.html"
+	?>
+	
+	<script>
+  		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  		ga('create', 'UA-74087442-1', 'auto');
+  		ga('send', 'pageview');
+	</script>
+	<link rel="stylesheet" href="css/fsm.css" />
+ </head>
+ <body>
+ <div class="container-fluid" style="height: 100%;">
+ 	<div class="row top text-left"> 		
+ 		<div class="container-fluid">
+			<div class="col-md-3">
+				<img src="img/main.png" class="logomain">
+			</div>
+			<div class="col-md-8">
+			
+			</div>
+			<div class="col-md-1">
+				<div class="pull-right">
+					<span class="fa fa-facebook-square social"></span>
+					<span class="fa fa-twitter-square social"></span>
+				</div>			
+			</div>
+ 		</div>
+ 	</div>
+ 	<div class="row middle">
+ 		<div class="col-md-4">
+ 			<div class="panel panel-default">
+ 				<div class="panel-heading">
+ 					<img src="img/attendance.png" class="img" alt="">
+ 				</div>
+ 				<div class="panel-body">
+ 					<p>
+ 						<img src="img/mobile_with_map.jpg" class="panel-image" alt="">
+ 						The solution to <strong style="color: black;">track employee attendance</strong> with their mobile phones.
+ 						Define Areas where your employees are allowed to clock in.
+ 						Control if they are actually at work in the designated areas.
+ 						Print the reports at the end of the month and attach them to the payslip.
+ 					</p>
+ 				</div>
+ 				<div class="panel-footer text-right">
+ 					<a href="attendance.php" class="btn btn-warning">FSM Attendance Info
+ 						<span class="glyphicon glyphicon-chevron-right">
+ 					</a>
+ 				</div>
+ 			</div>
+ 		</div>
+  		<div class="col-md-4">
+ 			<div class="panel panel-default">
+ 				<div class="panel-heading">
+ 					<img src="img/erp.png" class="img" alt="">
+ 				</div>
+ 				<div class="panel-body">
+ 					<p>
+ 						<img src="img/erp.jpg" class="panel-image" alt="">
+ 						<strong style="color: black;">The fully comprehensive solution.</strong>
+ 						FSM-Erp includes the attendance and fleet options in a PLUS version and more:  Workshop, Stores, Human Resources, Correspondence, Fuel logs, Vehicles, Reports etc.
+ 						A simple  but effective tool to manage your company.
+ 					</p>
+ 				</div>
+ 				<div class="panel-footer text-right">
+ 					<a href="" class="btn btn-warning">FSM-Erp Info
+ 						<span class="glyphicon glyphicon-chevron-right">
+ 					</a>
+ 				</div>
+ 			</div>
+ 		</div>
+  		<div class="col-md-4">
+ 			<div class="panel panel-default">
+ 				<div class="panel-heading">
+ 					<img src="img/fleet.png" class="img" alt="">
+ 				</div>
+ 				<div class="panel-body">
+ 					<p>
+ 						<img src="img/trucks.jpg" class="panel-image" alt="">
+ 						<strong style="color: black;">Manage your fleet of trucks.</strong> Vehicles, drivers, trips, document expiries and more.
+ 						Keep control on your vehicles and take action whene they are late at checkpoints or for deliveries.
+ 					</p>
+ 				</div>
+ 				<div class="panel-footer text-right">
+ 					<a href="" class="btn btn-warning">FSM Fleet Info
+ 						<span class="glyphicon glyphicon-chevron-right">
+ 					</a>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ 	<div class="row bottom">
+ 		<div class="col-md-3"></div>
+ 		<div class="col-md-2">
+ 			<a href="mailto:info@fsm.co.tz"><img src="img/envelope.png" class="contacts"> info@fsm.co.tz</a>
+ 		</div>
+ 		<div class="col-md-2">
+ 			<a href="tel:+255755468826"><img src="img/phone.png" class="contacts"> Tel. +255 755 46 88 26</a>
+ 		</div>
+ 		<div class="col-md-2">
+ 			<a href="skype: mazzofab.tz" ><img src="img/skype.png" class="contacts"> mazzofab.tz</a>
+ 		</div>
+ 		<div class="col-md-3"></div>
+ 	</div>
+ 	<div class="footer">
+ 		&copy; 2017 FSM Systems
+ 	</div>
+ </div>
+ </body>
